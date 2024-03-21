96330b263dfbf02f00b30d6532118f240dffc04b
AlexeyVolgograd <162894488+AlexeyVolgograd@users.noreply.github.com>
2024-03-21 22:24:03 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+x=38
+print('Дратуйте')
+if x<0:
+    print('Меньше нуля')
+print('Дотвидания')
+
+a, b=10, 5
+if a>b:
+    print('A>B')
+if a>b and a>0:
+    print('Успех')
+if (a>b) and(a>0 or b<1000):
+    print('Успех')
+if 5<b and b<10:
+    print('Успех')
+if'34'> '123':
+     print('Успех')
+if '123'>'34':
+    print('Успех')
+if [1,2]>[1,1]:
+    print('Успех')
+if '6'>5:
+    print('Успех')
+if[6,5]>'5':
+    print('Успех')
+if '6'!=5:
+    print('Успех')
+
